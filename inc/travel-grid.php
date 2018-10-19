bf1e680273b67052bcacd4baff621b00b14239ad
Sean Wieler <sbwieler@gmail.com>
2018-10-19 10:52:00 -0700

    first shortcode add

--- inc/travel-grid.php
+++ inc/travel-grid.php
@@ -2,8 +2,16 @@
 
 add_shortcode("travel_grid", "travel_grid_func");
 
-function travel_grid_func($atts) {
-    return "<p>IT WORKS!!</p>";
+function travel_grid_func($atts, $content=null) {
+    ob_start();
+    ?>
+    <ul>
+        <?php foreach ($atts as $attname ==> $attval): ?>
+            <li><strong><?php echo $attname; ?></strong> <?php echo $attval; ?></li>
+        <?php endforeach; ?>
+    </ul>
+    <p><?php echo $content; ?></p>
+    <?php
+    return ob_get_clean();
 }
-
 ?>
