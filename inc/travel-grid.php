bcd298ba5879622ab19657dca7be9ee6e8d55184
Sean Wieler <sbwieler@gmail.com>
2018-10-21 15:31:44 -0700

    added nav menu

--- inc/travel-grid.php
+++ inc/travel-grid.php
@@ -36,8 +36,7 @@ function travel_grid_func($atts, $content=null) {
     $travel_query = new WP_Query($args);
     ob_start();
     if ($travel_query->have_posts()):
-        $travel_query->the_title
-        echo the_title;
+        echo $travel_query->the_title
         else:
         ?>
         <p>No blogs found</p>
